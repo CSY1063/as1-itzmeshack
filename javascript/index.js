5b4ad4e87dbfd113b3cf1fb27b35ee11649ae60b
itzmeshack <meshackomon.dev@gmail.com>
2024-02-29 13:44:06 +0000

    making more changes

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,41 @@
+/* this whole  code open and close navigation tab on mobile and tablet */
 let sidemenu = document.getElementById('sidemenu');
 
+
+
+ /** this function openmenu() brings the content out */
 function openmenu(){
-    sidemenu.style.right = '1px';
+    sidemenu.style.right = '0';
 
-}
-
+};
+ /** this function closemenu() takes out the content out from
+     from the style.css */
 function closemenu(){
-    sidemenu.style.right = '-400px'
+    sidemenu.style.right = '-400px';
+    
+
+    
+
+};
+
+let x = document.getElementById('clicked');
+let for_mobile_nav =  document.getElementById('')
+    function suntoggle(){
+    if(window.innerWidth < 1028){
+        x.style.backgroundColor = '#ffffff';
+        
+       
+    }
+    
 }
+
+
+
+
+
+function moontoggle(){
+    x.style.backgroundColor = '#000000'
+}
+
+
+
